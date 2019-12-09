ba8d373bb11ab218e86f583d17f372b03db83ca6
Ihor Maikiv <imaykiv@yahoo.com>
2019-12-09 21:20:28 +0200

    Keil auto-generated ex_01_blinky project for Lecture #4 was added

---
+++ Lecture_04/ex_01_blinky/Objects/main.d
@@ -0,0 +1,9 @@
+.\objects\main.o: Source\main.c
+.\objects\main.o: D:\Program Files (x86)\Keil\ARM\PACK\Keil\STM32F3xx_DFP\1.1.0\Device\Include\stm32f30x.h
+.\objects\main.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\main.o: D:\Program Files (x86)\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\main.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\main.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\main.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\main.o: D:\Program Files (x86)\Keil\ARM\PACK\Keil\STM32F3xx_DFP\1.1.0\Device\Include\system_stm32f30x.h
---
+++ Lecture_04/ex_01_blinky/Objects/system_stm32f30x.d
@@ -0,0 +1,9 @@
+.\objects\system_stm32f30x.o: RTE\Device\STM32F303VC\system_stm32f30x.c
+.\objects\system_stm32f30x.o: D:\Program Files (x86)\Keil\ARM\PACK\Keil\STM32F3xx_DFP\1.1.0\Device\Include\stm32f30x.h
+.\objects\system_stm32f30x.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm4.h
+.\objects\system_stm32f30x.o: D:\Program Files (x86)\Keil\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_stm32f30x.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\system_stm32f30x.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\system_stm32f30x.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\system_stm32f30x.o: D:\Program Files (x86)\Keil\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Core\Include\mpu_armv7.h
+.\objects\system_stm32f30x.o: D:\Program Files (x86)\Keil\ARM\PACK\Keil\STM32F3xx_DFP\1.1.0\Device\Include\system_stm32f30x.h
